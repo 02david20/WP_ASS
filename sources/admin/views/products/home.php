f2ab458abdd82e1f6bd56f16112afa79bf6295c7
david <davidhuynh0222@gmail.com>
2022-12-04 23:19:12 +0700

    Merge main with products

--- sources/admin/views/products/home.php
+++ sources/admin/views/products/home.php
@@ -127,7 +127,7 @@
     <div class="modal-content">
       <form>
         <div class="modal-header">
-          <h4 class="modal-title">Delete User</h4>
+          <h4 class="modal-title">Xóa sản phẩm</h4>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
